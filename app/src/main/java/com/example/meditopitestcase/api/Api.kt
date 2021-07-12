105ea09befeb2ac73ed6441e8f4636c4e1a8c4a2
dtmdag <epje4m+417KSS>
2021-07-12 03:55:14 +0300

    initial project

---
+++ app/src/main/java/com/example/meditopitestcase/api/Api.kt
@@ -0,0 +1,1 @@
+package com.example.meditopitestcase.apiimport com.example.meditopitestcase.api.model.BaseMessageimport retrofit2.http.GETinterface Api {    @GET("chatdata-example.json")    suspend fun fetchMessages() : BaseMessage}
---
+++ app/src/main/java/com/example/meditopitestcase/ui/messagescreen/adapter/MessageViewHolder.kt
@@ -0,0 +1,1 @@
+package com.example.meditopitestcase.ui.messagescreen.adapterimport android.view.Viewimport androidx.recyclerview.widget.RecyclerViewabstract class MessageViewHolder<in T>(itemView: View) : RecyclerView.ViewHolder(itemView) {    abstract fun bind(item: T)}
